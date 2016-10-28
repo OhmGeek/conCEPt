a28b20de2a0c782979a351d4e60fa835fa693d65
Ryan Collins <ryan@ohmgeek.co.uk>
2016-10-28 19:53:12 +0100

    Added composer and started table generation with twig

--- reportmaker/TableMaker.php
+++ reportmaker/TableMaker.php
@@ -5,21 +5,27 @@
  * Date: 28/10/16
  * Time: 16:17
  */
+
 public class TableMaker {
+    private $twig;
     //todo use composer
     /**
      * TableMaker constructor.
      */
     public function __construct()
     {
-
+        //create a twig instance to use to render the page
+        require_once '../vendor/autoload.php';
+        $loader = new Twig_Loader_Filesystem('.views/');
+        $twig = new Twig_Environment($loader);
     }
 
     public function getTableFromXMLFile($filename) {
+        global $twig;
         $reader = new XMLConfigFileReader();
         $elemData = $reader->readConfigFile($filename);
-        $table = ""; //won't be a string.
-        //TODO: use composer along with the array to generate a table
+
+        $table = $twig->render('table.html',elemData);
 
         return $table;
     }
