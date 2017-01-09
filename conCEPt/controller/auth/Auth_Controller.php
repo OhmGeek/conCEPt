e284679bf435d2062f541fac6363725fb0cbed06
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-09 14:00:13 +0000

    Basic auth page

--- conCEPt/controller/auth/Auth_Controller.php
+++ conCEPt/controller/auth/Auth_Controller.php
@@ -7,20 +7,27 @@
  * Time: 12:22
  */
 require_once(__DIR__ . '/../../model/auth/UserAuthModel.php');
+
 class Auth_Controller
 {
     public static function auth_page($username) {
+        // create a user model
         $user_model = new UserAuthModel();
+
+        // create a twig loader
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../../view/auth');
+        $twig = new Twig_Environment($loader);
 
         if($user_model->isAdmin()) {
             // render admin page
-            echo "Admin";
+            return "Marker";
         }
         elseif($user_model->isMarker()) {
-            echo "Marker";
+            return "Marker";
         }
         else {
-            echo "Go away, you are not welcome here.";
+            $error_template = $twig->loadTemplate('403.twig');
+            return $error_template->render(array());
         }
     }
 }
--- conCEPt/public/index.php
+++ conCEPt/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-
+require_once(__DIR__ . '/../controller/auth/Auth_Controller.php');
 // deal with the odd installation we have going on
 $base = dirname($_SERVER['PHP_SELF']);
 
@@ -12,8 +12,9 @@ if(ltrim($base,'/')) {
 // main routing
 $router = new \Klein\Klein();
 
-$router->respond('GET', '/test', function() {
-    echo 'Hello World';
+$router->respond('GET', '/', function() {
+    $controller = new Auth_Controller();
+    return $controller::auth_page($_SERVER['REMOTE_USER']);
 });
 
 $router->onHttpError(function ($code, $router) {
