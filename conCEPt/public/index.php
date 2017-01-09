cab3f14e5488d1f972a99c7e15fea1a681f4e7e0
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-09 12:41:03 +0000

    Updated server config

--- conCEPt/public/index.php
+++ conCEPt/public/index.php
@@ -2,3 +2,29 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+// deal with the odd installation we have going on
+$base = dirname($_SERVER['PHP_SELF']);
+
+if(ltrim($base,'/')) {
+    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'],strlen($base));
+}
+
+// main routing
+$router = new \Klein\Klein();
+
+$router->respond('GET', '/test', function() {
+    echo 'Hello World';
+});
+
+$router->onHttpError(function ($code, $router) {
+    if ($code >= 400 && $code < 500) {
+        $router->response()->body(
+            'Oh no, a bad error happened that caused a '. $code
+        );
+    } elseif ($code >= 500 && $code <= 599) {
+        error_log('uhhh, something bad happened');
+    }
+});
+
+$router->dispatch();
+
