d61aae5a6f28f016b5666450411fb0348eb1eb3a
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-12 14:32:40 +0000

    Added CIS auth model and basic twig views

---
+++ conCEPt/model/userdata/UserDataDB.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 12/01/17
+ * Time: 12:16
+ */
+class UserDataDB
+{
+    public static function getDB()
+    {
+        return new PDO("mysql:host=mysql.dur.ac.uk;dbname=Pdcl0www_userdata",'nobody','');
+    }
+}
