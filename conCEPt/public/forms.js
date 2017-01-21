999f4ef3eaddef40bb36e25f5e0422e98e63eef6
stephenc29 <stephen.church@durham.ac.uk>
2017-01-21 20:10:57 +0000

    Add files via upload

--- conCEPt/public/forms.js
+++ conCEPt/public/forms.js
@@ -9,24 +9,35 @@ $(document).ready(function(){
 		}).on('keyup', function () {
 		  this.style.height = 'auto';
 		  this.style.height = (this.scrollHeight) + 'px';
-		  console.log(this.style.height);
 		});
 		
 	//Ajax post request made when form is submitted
 	$("form").submit(function(){
 		$data = $(this).serializeArray();
 		$jsonData = {};
+		$numberOfSections = -1; //Will ignore general comments section
 		$.each($data, function(){
-			$jsonData[this.name] = this.value
+			$jsonData[this.name] = this.value;
+			$numberOfSections += 1;
 		});
-		
-/* 		$.post("index.php", jsonData, function(response){
-			if response.hasOwnProperty("error"){
+		$jsonData["documentID"] = $("form").attr("id");
+		//DON'T KNOW HOW TO GET THE STORE TYPE FROM THE FORM YET
+		$jsonData["storeType"] = "save";
+		$jsonData["numberOfSections"] = $numberOfSections/2;
+		console.log($jsonData);
+
+ 		$.post("index.php", $jsonData, function(response){
+			if (response.hasOwnProperty("error")){
 				//alert with response["error"];
+				console.log("Error");
 			}else{
 				//alert with response["success"];
+				console.log("Worked");
+				console.log(response);
+				response = $.parseJSON(response);
+				console.log(response["sections"]);
 			}
-		} */
+		});
 	});
 	
 	//If all inputs and textareas are filled, enable submit button
@@ -36,14 +47,12 @@ $(document).ready(function(){
 			return $.trim($(this).val()).length == 0;
 		}).length==0;
 		
-		console.log(inputsFilled);
 		
 		//Boolean value - true if no textarea fields are empty
 		var textareasFilled = $("textarea").filter(function(){
 			return $.trim($(this).val()).length == 0;
 		}).length==0;
 		
-		console.log(textareasFilled);
 		
 		//If inputsFilled and textareasFilled,
 		//remove disabled attribute from Submit input
