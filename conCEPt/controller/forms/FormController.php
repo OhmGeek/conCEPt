893613d0b5a15d5ffb29a085f0987160f7762b89
OhmGeek <ryan@ohmgeek.co.uk>
2017-01-22 17:49:09 +0000

    Updated stuff

--- conCEPt/controller/forms/FormController.php
+++ conCEPt/controller/forms/FormController.php
@@ -1,34 +1,23 @@
 <?php
 
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../model/forms/FormModel.php');
 
-Class FormController
+
+class FormController
 {
     function __construct()
     {
         $this->generateEditableForm();
     }
 
-    function generateEditableForm()
+    function generateEditableForm($baseFormID)
     {
-        $loader = new Twig_Loader_Filesystem('../view/formPage/');
+		$form_model = new FormModel();
+		$form_data = $form_model->getBlankFormByBaseID($baseFormID);
+	
+		$loader = new Twig_Loader_Filesystem('../view/formPage/');
         $twig = new Twig_Environment($loader);
-
-
-
-        $markingCriteria = /*GET FROM SQL*/ array(array('criteria'=> "Did the student submit any work? (95%)",
-                                                        'markID'=> "1111",
-                                                        'markReadOnly'=>"readonly",
-                                                        'mark'=>"50%",
-                                                        'rationaleID'=>"2222",
-                                                        'rationaleReadOnly' => "readonly",
-                                                        'rationale' => "I forgot to check so I'm hedging my bets"),
-                                                  array('criteria'=> "Is it any good? 5%",
-                                                        'markID'=> "3333",
-                                                        'markReadOnly'=>"readonly",
-                                                        'mark'=>"0%",
-                                                        'rationaleID'=>"4444",
-                                                        'rationaleReadOnly' => "readonly",
-                                                        'rationale' => "It's unlikely") );
 
         $title = "Design Report";
         $markerType = "Supervisor";
@@ -47,6 +36,6 @@ Class FormController
                                         'studentName' => $studentName));
 
         $template = $twig->loadTemplate("mainFormPage.twig");
-        print($template->render(array('form'=> $form)));
+        return $template->render(array('form'=> $form));
     }
 }
--- conCEPt/model/forms/FormModel.php
+++ conCEPt/model/forms/FormModel.php
@@ -12,25 +12,50 @@ class FormModel {
 
 		public function getFormByID($formID) {
 				$statement = $this->db->prepare(
-						"SELECT Section.Sec_Order, Section.Sec_Name, Section.Sec_Percent, Section.Sec_Criteria, SectionMarking.Comment, SectionMarking.Comment, SectionMarking.Mark
+						"SELECT Section.Sec_Name, Section.Sec_Percent, Section.Sec_Criteria, SectionMarking.Comment, SectionMarking.Comment, SectionMarking.Mark
 						FROM SectionMarking
 						JOIN Section ON Section.Sec_ID = SectionMarking.Sec_ID
-						WHERE SectionMarking.Form_ID = :formID");
+						WHERE SectionMarking.Form_ID = :formID
+						ORDER BY Section.Sec_Order");
 				$statement->bindValue(":formID", $formID,PDO::PARAM_STR);
 				$statement->execute();
-
-				return $statement->fetchAll(PDO::FETCH_ASSOC);
+				$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+				$output = array();	
+				// now go through and change formatting
+				foreach($data as $row) {
+					$rowArray = array(
+							'criteria' => row['Sec_Criteria'],
+							'markID' => "1111",
+							'markReadOnly' => "readonly",
+							'mark' => row['Mark'],
+							'rationaleID' => "1",
+							'rationaleReadOnly' => "readonly",
+							'rationale' => row['Comment']
+					);
+					array_push($output,$rowArray);
+				}	
+				
+				return $output;
 		}
 
 		public function getBlankFormByBaseID($bFormID) {
 				$statement = $this->db->prepare(
-						"SELECT Sec_Name, Sec_Criteria, Sec_Percent, Sec_Order
+						"SELECT Sec_Name, Sec_Criteria, Sec_Percent
 						 FROM Section
-						 WHERE Section.BForm_ID = :baseFormID");
+						 WHERE Section.BForm_ID = :baseFormID
+						 ORDER BY Sec_Order");
 				$statement->bindValue(":baseFormID", $bFormID,PDO::PARAM_STR);
 				$statement->execute();
 
-				return $statement->fetchAll(PDO::FETCH_ASSOC);
+				$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+				// now go through the data formatting it correctly
+
+
+
+
+
+				return $data
 
 
 		}
