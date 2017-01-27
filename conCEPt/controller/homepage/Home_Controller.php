8272a6ba6a3272cf538c80384a911e982b039740
CS-SEG04 C.S <cs.seg04@durham.ac.uk>
2017-01-27 13:02:44 +0000

    Fixed errors

--- conCEPt/controller/homepage/Home_Controller.php
+++ conCEPt/controller/homepage/Home_Controller.php
@@ -21,7 +21,6 @@ class MainPageController
 						$submitted_msg = "Submitted";
 					}
 					$form = array('formID'=> array(
-<<<<<<< HEAD
 						'title' => $value['Form_Title'],
 						'submitted' => $submitted_msg,
 						'submitted_link' => 'todo: link',
@@ -34,22 +33,12 @@ class MainPageController
 					// now add this form to the list of forms for the student
 					array_push($forms,$form);
 
-=======
-						'title' => $form['title'],
-						'submitted' => $form['submittedmsg'],
-						'submitted_link' => $form['completelink'],
-						'shadow_submitted' => $form['complete2'],
-						'shadow_link' => $form['complete2link'],
-						'linkMerged' => $form['mergedlink'],
-						'type' => $form['type'],
-					));
->>>>>>> c3e07277d3a814efcdbb5ed4cfcda7a56e5340e7
 			}
 			$student = array(
 					'name' => "Hi",
 					'forms' => $forms
 			);
-			$array_push(twig_data);
+			array_push($twig_data,$student);
 		}
 		// now go through all the data gathered, rendering the page itself
 		
