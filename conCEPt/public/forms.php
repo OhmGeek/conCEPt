1ed2559c3f9af63e10b4685ff0200cd65eff7e03
CS-SEG04 C.S <cs.seg04@durham.ac.uk>
2017-02-02 18:41:28 +0000

    Initial generation works

---
+++ conCEPt/public/forms.php
@@ -0,0 +1,27 @@
+<?php
+	require_once '../vendor/autoload.php';
+	include '../model/db.php';
+	include '../controller/saveSubmitController.php';
+	include '../controller/formSelectionController.php';
+	include '../controller/FormController.php';
+	include '../controller/historyController.php';
+	include '../controller/navbarController.php';
+	
+	$route = $_GET["route"];
+
+	
+	if ($route == "send"){
+		$test = new SaveSubmitController($_POST);
+	}elseif ($route == "receive"){
+		$formID = $_GET["id"];
+		$test = new FormController($formID);
+	}elseif ($route == "select"){
+		$formTypeID = $_GET["typeId"];
+		$test = new formSelectionController();
+		$test->generateSelectionPage($formTypeID);
+	}elseif($route == "history"){
+		$test = new HistoryController();
+	}elseif($route == "navbar"){
+		$test = new navbarController();
+		print_r($test->generateNavbarHtml());
+	}
