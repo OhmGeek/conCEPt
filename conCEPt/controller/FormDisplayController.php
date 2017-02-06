42c8f2e04174307a6ea3ec4604be130a61a2edd9
stephenc29 <stephen.church@durham.ac.uk>
2017-02-06 14:26:11 +0000

    Update FormDisplayController.php

--- conCEPt/controller/FormDisplayController.php
+++ conCEPt/controller/FormDisplayController.php
@@ -36,10 +36,13 @@ Class FormDisplayController
         //Get HTML for main form
         $form = $this->generateForm($formID);
 
+        print_r($form);
         //Generat HTML for navbar
         $navbar = new NavbarController();
         $navbar = $navbar->generateNavbarHtml();
 
+        print_r($navbar);
+        
         //Generate main page with mainFormPage.twig
         $template = $this->twig->loadTemplate("mainFormPage.twig");
         print($template->render(array('title' => $formTitle, 'navbar' => $navbar, 'form' => $form)));
