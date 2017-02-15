58f1f9cfc18d22fae83410396b0fc446e513cb80
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2017-02-15 16:05:23 +0000

    Add all functions to model
    Added lots of SQL functions to EditCriteriaModel

--- conCEPt/controller/EditCriteriaController.php
+++ conCEPt/controller/EditCriteriaController.php
@@ -1,2 +1,27 @@
 <?php
 
+use Concept/Model/EditCriteriaModel;
+
+Class EditCriteriaController
+{
+    function __construct()
+    {
+        if(isset($_GET['form']))
+        {
+            $form = $_GET['form'];
+            $this->editCritria($form);
+        }
+        else
+        {
+            $this->displayFormList();
+        }
+    }
+
+    function editCriteria($form)
+    {
+        $model = new EditCriteriaModel();
+        $criteria = $model->getFormCriteria($form);
+
+        
+    }
+}
---
+++ conCEPt/model/EditCriteriaModel.php
@@ -0,0 +1,107 @@
+<?php
+
+use Concept/Model/DB;
+
+class EditCriteriaModel
+{
+    function __construct()
+    {
+
+    }
+
+    function getFormCriteria($form)
+    {
+
+        /*IDs for each base form. 1 design 2 presentation 3 project paper 4 project poster 5 oral*/
+        switch ($form) {
+            case 'Design':
+                $bFormID = 1;
+                break;
+            case 'Presentation':
+                $bFormID = 2;
+                break;
+            case 'Project Paper':
+                $bFormID = 3;
+                break;
+            case 'Project Poster':
+                $bFormID = 4;
+                break;
+            case 'Oral':
+                $bFormID = 5;
+                break;
+
+            default:
+                /*if an invalid form is asked for do nothing*/
+                /*maybe add a 404 for this?*/
+                exit;
+        }
+        $db = DB::getDB();
+        $statement = $db->prepare("SELECT * 
+                                   FROM `Section`
+                                   WHERE `BForm_ID` = :BformID
+                                   ORDER BY `Sec_Order`");
+
+
+        $statement->bindValue(':BFormID', $bFormID, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    function setFormCriteria($secName, $secCrit, $secPerc, $bFormID, $secOrder)
+    {
+        $db = DB::getDB();
+        $statement = $db->prepare("UPDATE `Section` 
+                                   SET `Sec_Name`= :Sec_Name ,`Sec_Criteria`= :Sec_Criteria ,`Sec_Percent`= :Sec_Percent
+                                   WHERE `BForm_ID` = :BFormID AND `Sec_Order`= :Sec_Order");
+
+        $statement->bindValue(':Sec_Name', $secName, PDO::PARAM_STR);
+        $statement->bindValue(':Sec_Criteria', $secCrit, PDO::PARAM_STR);
+        $statement->bindValue(':Sec_Percent', $secPerc, PDO::PARAM_STR);
+
+        $statement->bindValue(':BFormID', $bFormID, PDO::PARAM_INT);
+        $statement->bindValue(':Sec_Order', $secOrder, PDO::PARAM_INT);
+
+
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+    }
+    function createFormCriteria($secName, $secCrit, $secPerc, $bFormID, $secOrder)
+    {
+        $db = DB::getDB();
+        $statement = $db->prepare("INSERT INTO `Section`(`Sec_Name`, `BForm_ID`, `Sec_Criteria`, `Sec_Percent`, `Sec_Order`) VALUES (:Sec_Name,:BFormID,:Sec_Criteria,:Sec_Percent,:Sec_Order)");
+
+        $statement->bindValue(':Sec_Name', $secName, PDO::PARAM_STR);
+        $statement->bindValue(':Sec_Criteria', $secCrit, PDO::PARAM_STR);
+        $statement->bindValue(':Sec_Percent', $secPerc, PDO::PARAM_STR);
+
+        $statement->bindValue(':BFormID', $bFormID, PDO::PARAM_INT);
+        $statement->bindValue(':Sec_Order', $secOrder, PDO::PARAM_INT);
+
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function deleteFormCriteria($bFormID, $secOrder)
+    {
+        $db = DB::getDB();
+        $statement = $db->prepare("DELETE FROM `Section` 
+                                   WHERE `BForm_ID` = :BFormID AND `Sec_Order` = :secOrder");
+
+        $statement->bindValue(':BFormID', $bFormID, PDO::PARAM_INT);
+        $statement->bindValue(':Sec_Order', $secOrder, PDO::PARAM_INT);
+
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function countNoSections($bFormID)
+    {
+        $db = DB::getDB();
+        $statement = $db->prepare("SELECT COUNT(`Sec_ID`) AS Num_Sections
+                                   FROM `Section` 
+                                   WHERE `BForm_ID` = :BFormID");
+
+        $statement->bindValue(':BFormID', $bFormID, PDO::PARAM_INT);
+    }
+}
