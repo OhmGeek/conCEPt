b65453971aa57c26090ba879d868ebcc0b766649
OhmGeek <ryan@ohmgeek.co.uk>
2017-02-27 14:58:42 +0000

    Updated Composer Autoloader to use namespaces

--- conCEPt/vendor/composer/autoload_real.php
+++ conCEPt/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit69c39a7b514063208f45cb1d751f7c2f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit69c39a7b514063208f45cb1d751f7c2f', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
